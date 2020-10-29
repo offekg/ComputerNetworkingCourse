6943640a8716fc00bfda974ab4b8888db6021e00
Offek Gil <offekg@gmail.com>
2020-10-30 00:47:34 +0200

    basic sending of heaps from server to client with struct
    also made basic client-server sending test function

--- nim-client.py
+++ nim-client.py
@@ -1,7 +1,48 @@
+#!/usr/bin/python3
+import sys
 import socket
+import struct
 
 
-def open_connection():
-    with socket.socket() as soc:
-        soc.bind(6644)
-        soc.listen()
+def nim_game_client(my_host, my_port):
+    game_not_over = True
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
+        try:
+            soc.connect((my_host, my_port))
+        except socket.error as e:
+            print(e.strerror)
+        while game_not_over:
+            output = soc.recv(1024)
+            heaps = struct.unpack(">iii", output)
+            print(heaps)
+            game_not_over = False
+
+
+
+def connection(my_host, my_port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
+        try:
+            soc.connect((my_host, my_port))
+        except socket.error as e:
+            print(e.strerror)
+
+        try:
+            soc.send("Hello".encode())
+        except socket.error as e:
+            print(e.strerror)
+
+        output = ""
+        try:
+            output = soc.recv(1024)
+        except socket.error as err:
+            print(err.strerror)
+
+        print(output)
+
+
+#host = sys.argv[1]
+#port = sys.argv[2]
+#path = sys.argv[3]
+
+nim_game_client("127.0.0.1", 6644)
--- nim-server.py
+++ nim-server.py
@@ -1,12 +1,69 @@
-from socket import *
+#!/usr/bin/python3
+import socket
+import sys
+import struct
 
 
+def nim_game_server(my_port):
+    heaps = [3,4,6]
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind(('', my_port))
+        except socket.error as e:
+            print(e.strerror)
+
+        try:
+            s.listen(5)
+        except socket.error as e:
+            print(e.strerror)
+
+        conn_sock = None
+        try:
+            conn_sock, address = s.accept()
+        except socket.error as e:
+            print(e.strerror)
+
+        with conn_sock:
+            data = struct.pack(">iii", heaps[0], heaps[1], heaps[2])
+            try:
+                conn_sock.send(data)
+            except socket.error as e:
+                print(e.strerror)
+
+def accept_connection(my_port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind(('', my_port))
+        except socket.error as e:
+            print(e.strerror)
+
+        try:
+            s.listen(5)
+        except socket.error as e:
+            print(e.strerror)
+
+        conn_sock = None
+        try:
+            conn_sock, address = s.accept()
+        except socket.error as e:
+            print(e.strerror)
+
+        with conn_sock:
+            msg = ""
+            try:
+                msg = conn_sock.recv(1024).decode()
+            except socket.error as e:
+                print(e.strerror)
+
+            print("Message from client is:",msg)
+
+            data = "Goodbye"
+            try:
+                conn_sock.send(data.encode())
+            except socket.error as e:
+                print(e.strerror)
 
 
-def open_connection():
+#port = sys.argv[1]
 
-    ListeningSocket = socket(AF_INET, SOCK_STREAM)
-    ListeningSocket.bind(("", 80))
-    ListeningSocket.listen(5)  # Socket becomes listening
-    (conn_sock, address) = ListeningSocket.accept()
-    byt_obj = conn_sock.recv(1024)
+nim_game_server(6644)
