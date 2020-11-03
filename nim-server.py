7994fa9ac682729bdc38d3d44a2d7c2dd403012c
yaellerech <48100558+yaellerech@users.noreply.github.com>
2020-11-03 19:47:42 +0200

    added notes

--- nim-server.py
+++ nim-server.py
@@ -33,10 +33,14 @@ def server_move():
             max_index = i
     heaps[max_index] -= 1
 
-
+# this function is responsible for the server socket connection and the server game logic.
+# it starts a socket, with socket, bind and listen commands.
+# when a client tries to connect, it accepts a single connection and the game begins.
+# when the connection is closed, returns to listening until a new client connect.
+# in case of an error, the server closes the connection and the function returns.
 def nim_game_server(my_port,n_a,n_b,n_c):
-    output = None
 
+    # creating a socket with socket, bind and listen commands.
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
         try:
             soc.bind(('', my_port))
@@ -53,77 +57,93 @@ def nim_game_server(my_port,n_a,n_b,n_c):
             return
 
         conn_sock = None
+        output = None
 
+        # while the server is up, it is waiting fo a client to connect to him.
         while True:
             try:
                 conn_sock, address = soc.accept()
             except socket.error as e:
+                # Failed specific "accept". continue to try again next iteration
                 print(e.strerror)
-                continue  # Failed specific "accept". continue to try again next iteration
+                continue
 
             with conn_sock:
+                # created a connection with a client. the games starts with full heaps.
+                # for each new connection we will restart the heaps sizes
                 heaps[0] = n_a
                 heaps[1] = n_b
-                heaps[2] = n_c     # for each new connection we will restart the heaps sizes
-                status = PLAYERS_TURN    # 0=no one, 1=player, 2=server
+                heaps[2] = n_c
 
-                while True:  # While client is still playing / connection is alive
-                #   1) Send heap values and game status
+                #the game starts with the player's turn
+                status = PLAYERS_TURN
+
+                while True:
+                    # While client is still playing / connection is alive
+                    # 1) Send to the client the current heap values and game status
                     data = struct.pack(">iiii", heaps[0], heaps[1], heaps[2], status)
                     send_status = send_all(conn_sock, data)
                     if send_status == 0:
-                        # there was an error while sending the data to the client
+                        # there was an error while sending the data to the client. the game ends with this client.
                         break
                     elif send_status == 2:
-                        # there was a connection errpr from client
+                        # there was a connection error from client, we print a specific message and end the game.
                         print("Disconnected from client")
                         break
 
                     if status != PLAYERS_TURN:
-                        break  # game is over - close connection
+                        # the game is over - close connection with the current client.
+                        break
 
-                #   2) accept players move and return response
+                    # 2) accept players move and return response
                     move = recv_all(conn_sock, ">ii")
-                    if move == 2:  # there was a connection error during recv
-                        # If recv_all failed, close connection
+                    if move == 2:
+                        # there was a connection error from client, we print a specific message and end the game.
                         print("Disconnected from client")
                         break
-                    elif move == 0:  # there was an error during recv
-                        # If recv_all failed, close connection
+                    elif move == 0:
+                        # there was an error while receiving the data from the client. the game ends with this client.
                         break
 
                     move = struct.unpack(">ii", move)
 
-                    if move[0] == QUIT:  # Client closed the game
+                    if move[0] == QUIT:
+                        # Client ended the game.
                         break
                     elif move[0] == ILLEGAL_HEAP_INPUT:
+                        # the move received from the player was illegal.
                         server_response = PLAYER_ILLEAGL_MOVE
                     else:
                         if is_legal_move(move):
+                            # the player's move is legal so we update the heaps accordingly.
                             heaps[move[0]] -= move[1]
                             server_response = PLAYER_MOVE_ACCEPTED
                             if is_win():
                                 status = PLAYER_WINS
-                        else:  # Illegal player move
+                        else:
+                            # Illegal player move
                             server_response = PLAYER_ILLEAGL_MOVE
 
+                    # send the client if the player's move was accepted or not.
                     data = struct.pack(">i", server_response)
                     send_status = send_all(conn_sock, data)
                     if send_status == 0:
-                        # there was an error while sending the data to the client
+                        # there was an error while sending the data to the client. the game ends with this client
                         break
                     elif send_status == 2:
-                        # there was a connection errpr from client
+                        # there was a connection error from client, we print a specific message and end the game.
                         print("Disconnected from client")
                         break
 
-                #   3) make servers move
+                #   3) play the server's move if the player didn't win yet.
                     if status != PLAYER_WINS:
                         server_move()
                         if is_win():
                             status = SERVER_WINS
 
-
+# this function starts the server
+# gets the arguments for the server program and wraps send them to the nim_game_server function.
+# wrapps the nim_game_server function in case of an error.
 def start_server():
     if len(sys.argv) == 5:
         port = sys.argv[4]
